0e17889f7fcff44eae2d228c77172e264ac7ab6b
ŅatalyKisialiova <nataly.kisialiova@gmail.com>
2025-01-03 18:39:11 +0200

    feat: change resolving mac-address.js task

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -18,11 +18,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getMatrixElementsSum(matrix) {
   let sum = 0;
-  matrix.forEach((row, indexRow) => row.forEach((item, indexCel) => {
-  if (matrix[indexRow - 1] && matrix[indexRow - 1][indexCel] !== 0 || indexRow === 0) {
-    sum += item;
-  }  
-  }));
+  // matrix.forEach((row, indexRow) => row.forEach((item, indexCel) => {
+  // if (matrix[indexRow - 1] && matrix[indexRow - 1][indexCel] !== 0 || indexRow === 0) {
+  //   sum += item;
+  // }  
+  // }));
+  for (let  column = 0;  column < matrix[0].length;  column += 1) {
+    for (let row = 0; row < matrix.length; row += 1) {
+      if(matrix[row][column] !== 0) {
+        sum += matrix[row][column]; 
+      } else {
+        break;
+      }
+    }
+  }
   return sum;
 }
 
